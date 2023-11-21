463344dd9e74a21ee8597a4f747c71721f67c956
Damien Vesper <ldamienvesper@gmail.com>
2023-11-20 18:08:25 -0600

    this will kill

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,10 +1,10 @@
 export const Config = {
     regions: {
-        dev: { name: "Local Server", address: "dv.suroi.io", https: true },
-        na: { name: "North America", address: "na.suroi.io", https: true },
-        eu: { name: "Europe", address: "eu.suroi.io", https: true },
-        sa: { name: "South America", address: "sa.suroi.io", https: true },
-        as: { name: "Asia", address: "as.suroi.io", https: true }
+        dev: { name: "Local Server", address: "dv.suroi.io", https: true } //,
+        // na: { name: "North America", address: "na.suroi.io", https: true },
+        // eu: { name: "Europe", address: "eu.suroi.io", https: true },
+        // sa: { name: "South America", address: "sa.suroi.io", https: true },
+        // as: { name: "Asia", address: "as.suroi.io", https: true }
     },
     defaultRegion: "dev"
 } satisfies ConfigType as ConfigType;
